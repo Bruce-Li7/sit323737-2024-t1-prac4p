24a08cd5c5bdc702229f50d5b122e4929f5c885f
Bruce-Li7 <lijiaqi7788@gmail.com>
2024-05-21 15:50:00 +1000

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ var app = express();
 const PORT = process.env.PORT || 3000; // Define port which is on 3000
 
 //defined add,subtract,multiply and divide
-const add = (n1,n2) =>{return n1+n2};
-const subtract = (n1,n2) =>{return n1-n2};
-const multiply = (n1,n2) =>{return n1*n2};
-const divide = (n1,n2) =>{return n1/n2};
-//winston config
+const add = (n1,n2) =>{return n1+n2}; //Add: add(n1, n2) returns the sum of n1 and n2.
+const subtract = (n1,n2) =>{return n1-n2}; //Subtract: subtract(n1, n2) returns the difference between n1 and n2.
+const multiply = (n1,n2) =>{return n1*n2}; //Multiply: multiply(n1, n2) returns the product of n1 and n2
+const divide = (n1,n2) =>{return n1/n2}; //Divide: divide(n1, n2) returns the quotient of n1 divided by n2
+//A winston logger is configured with console and file transports.
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
@@ -33,15 +33,18 @@ if(process.env.NODE_ENV !== 'production'){
     }));
 }
 //add
+///add takes n1 and n2 as query parameters, performs addition, and returns the result.
 app.get("/add", (req, res) => {
     try {
         const n1 = parseFloat(req.query.n1);
         const n2 = parseFloat(req.query.n2);
         if (isNaN(n1)) {
+            //Errors are logged and thrown if input parameters are not numbers.
             logger.error("n1 is incorrect");
             throw new Error("n1 incorrect");
         }
         if (isNaN(n2)) {
+            //Errors are logged and thrown if input parameters are not numbers.
             logger.error("n2 is incorrect");
             throw new Error("n2 incorrect");
         }
@@ -98,6 +101,7 @@ app.get("/multiply", (req, res) => {
     } catch (error) {
         
         console.log(error);
+        //If n1 or n2 are not numbers, a 300 status code and error message are returned.
         res.status(300).json({ statuscode: 300, msg: error.toString() });
 
     }
